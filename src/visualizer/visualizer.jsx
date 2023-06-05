d9b4a3d56a26f45a5fd3cf213ffa0bf35f1ed276
chaeriz <chrisobt@outlook.com>
2023-06-05 16:30:33 +0200

    reset btn

--- src/visualizer/visualizer.jsx
+++ src/visualizer/visualizer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-
+import "./visualizer.css";
 export default class Visualizer extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             array: [],
         };
+        this.resetArray = this.resetArray.bind(this);
+        this.sort = this.sort.bind(this);
     }
     componentDidMount(){
         this.resetArray();
@@ -13,22 +15,30 @@ export default class Visualizer extends React.Component {
     resetArray() {
         let a = [];
         for (let i = 0; i < 50; i++ ) {
-        a.push(this.randomInt());
+        a.push(Math.floor(Math.random(5)*500 +5));
         }
         this.setState( {array: a });
-    } 
-    randomInt() {
-        return Math.floor(Math.random(5)*100 +5);
+    }
+    sort() {
+
     }
     render() {
         const { array } = this.state;
 
         return (
-            <div>
+            <div className="container">
+                <div className="control">
+                    <button onClick={this.resetArray}>Reset</button>
+                    <button >Sort</button>
+                </div>
+                <div className="display">
+                
                 {array.map((item, index) => (
-                    <div key={index}>{item}</div>
+                    <div className="obj" style={{height: `${item}px`}} key={index}>{item}</div>
                 ))}
             </div>
+            </div>
+            
         );
     }
 }
